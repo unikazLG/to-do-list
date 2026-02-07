d986cbd4f68c1b0f91e15895fbb1f1a9b14ab562
Kajol Gupta <kajolgupta1395@gmail.com>
2026-02-07 18:10:02 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,39 @@
+let todoList=[
+    {item:'buy milk',
+        dueDate:'4/10/2023'},
+    {item:'buy choclate',
+        dueDate:'4/10/2023'}
+];
+displayItems();
+
+
+function addTodo(){
+    let inputElement=document.querySelector('#todo-input');
+    let dateElement=document.querySelector('#todo-date');
+    let  todoItem=inputElement.value;
+    let todoDate=dateElement.value;
+    todoList.push({item:todoItem,dueDate:todoDate});
+    inputElement.value='';
+    dateElement.value='';
+    displayItems();
+    
+
+}
+function displayItems(){
+    let containerElement=document.querySelector('.todo-container');
+    let  newHTML=``;
+    for(let i=0;i<todoList.length;i++){
+        // let item=todoList[i].item;
+        // let dueDate=todoList[i].dueDate;
+        let{item,dueDate}=todoList[i];
+        newHTML+=`
+        
+        <span>${item}</span>
+        <span>${dueDate}</span>
+
+        <button class='btn-delete' onclick="todoList.splice(${i},1); displayItems();">DELETE</button>
+        `;
+   
+    }
+    containerElement.innerHTML=newHTML;
+}
